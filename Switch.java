609f710cb6adac3a4af93843966434ec1d56d9ca
hcc <hcc@gmail.com>
2025-01-13 14:02:05 +0100

    Segunda clase con atributos y constructores

---
+++ Switch.java
@@ -0,0 +1,9 @@
+public class Switch {
+    private int pulsaciones;
+    private String tipoSwitch;
+    
+    public Switch(String switchTeclado){
+        pulsaciones = 0;
+        tipoSwitch = switchTeclado;        
+    }
+}
