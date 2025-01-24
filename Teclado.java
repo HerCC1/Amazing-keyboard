9461969aa7ecf7f01c5877aa7683b7610df5c724
Daniel <danielmateos684@gmail.com>
2025-01-24 10:00:38 +0100

    Metodos getter primera clase

--- Teclado.java
+++ Teclado.java
@@ -5,8 +5,21 @@ public class Teclado
     private Switch tipoSwitch;
     
     
-    public Teclado(int tamanoTeclado, String lenguaje){
+    public Teclado(int tamanoTeclado, String lenguaje, String switchTeclado){
         tamanoEnPorcentaje = tamanoTeclado;
         idioma = lenguaje;
+        tipoSwitch = new Switch(switchTeclado);
+    }
+    
+    public int getTamanoTeclado() {
+        return tamanoEnPorcentaje;
+    }
+    
+    public String getIdioma() {
+        return idioma;
+    }
+    
+    public Switch getTipoSwitch() {
+        return tipoSwitch;
     }
 }
