5eafda7c8b93cd7fd916abd8bd71a9a2dea00536
hcc <hcc@gmail.com>
2025-01-24 09:41:17 +0100

    Añadimos constructor de la clase

--- Teclado.java
+++ Teclado.java
@@ -3,4 +3,10 @@ public class Teclado
     private int tamanoEnPorcentaje;
     private String idioma;
     private Switch tipoSwitch;
+    
+    
+    public Teclado(int tamanoTeclado, String lenguaje){
+        tamanoEnPorcentaje = tamanoTeclado;
+        idioma = lenguaje;
+    }
 }
